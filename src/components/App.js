0f5faf3284bf95af746f866fe2ab4f9b46641c55
Hitoishi Das <hitoishi.das@gmail.com>
2020-06-01 01:56:08 +0530

    Added start page and fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Route, BrowserRouter as Router} from 'react-router-dom'
+import Start from './Start/Start'
 
 export default ()=>{
     return(
         <Router>
+            <Route exact path="/" component={Start} />
         </Router>
     )
 }
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,9 +1,30 @@
-import {React} from 'react';
+import React from 'react';
+import './Start.css'
 
 export default ()=>{
     return(
         <div>
-            
+            <div className="row options-field">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <div className="offset-md-5 offset-lg-5 offset-xl-5 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                        <p>Choose a theme</p>
+                    </div>
+                </div>
+                <br />
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <div className="offset-md-5 offset-lg-5 offset-xl-5 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                        <input type="radio" value="NARUTO" id="opt1"/>
+                        <label htmlFor="opt1" className="naruto-title">Naruto</label>
+                    </div>
+                </div>
+                <br />
+                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <div className="offset-md-5 offset-lg-5 offset-xl-5 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                        <input type="radio" value="SIMPSON" id="opt2"/>
+                        <label htmlFor="opt2" className="naruto-title">Simpsons</label>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
